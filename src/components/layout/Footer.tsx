ec0bc16e0aa1a1f5c755e089e717784f800e8a20
roh27 <71496802+roh27@users.noreply.github.com>
2021-04-28 10:25:08 +0530

    Final

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * React component for the Footer Section.
+ */
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&#169; 2020 Rohith Kanna L M - source code on <a href="www.google.com">Github</a></p>
+    </footer>
+  )
+}
